c557c3d6770581fff2219b0f6ff74e9c411368c8
Pavan Harali <pavanharali018@gmail.com>
2026-09-16 01:52:11 +0530

    DOM added

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("hello");
+let searchForm =document.querySelector("#search-form");
+let usernameInput=document.querySelector("#username-input");
+let profileContainer =document.querySelector("#profile-container");
+let profileAvatar =document.querySelector("#profile-avatar");
+let profileName =document.querySelector("#profile-name");
+let profileBio =document.querySelector("#profile-bio");
+let profileLink =document.querySelector("#profile-link");
+let selector=document.querySelectorAll(".stat-value")
+
+
